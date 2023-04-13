c62a928d4fc6a3d03e353d12f4bbebb5de46cb8e
Mustak Ahmed <72276954+Mustak-Ahmed@users.noreply.github.com>
2023-04-13 19:00:59 +0530

    Update app.py
    message updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Hi from Docker </h2>'
+    return '<h1>Hello from Flask </h2>'
 
 
 if __name__ == "__main__":
